7a43a71a8501f0734305d85021acb64dd73b59e9
haru <haruki1472@icloud.com>
2022-08-10 20:39:54 +0900

    swift first commit

---
+++ Shared/app_memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_memoApp.swift
+//  Shared
+//
+//  Created by 大場春希 on 2022/08/10.
+//
+
+import SwiftUI
+
+@main
+struct app_memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
